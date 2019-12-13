8a4a49a1a5e9427e892c353f7a221ad6fd13bb7e
Ismael Hasan <ismael.hasan@live.com>
2019-12-13 20:14:21 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,8 @@
 def square_array(array)
   result = Array.new
-  num = 0
- 
-  array.each do
-    result << array[num]**2
-    num += 1
+
+  array.each do |num|
+    result << num**2
   end
   result
 end
